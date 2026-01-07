3dd898631f4d2fa23c85d4d658674515c70e630c
MichelleIhetu <michellekihe@gmail.com>
2026-01-06 20:08:43 -0500

    Updated my evals dashboard. Still debugging

--- c3an-building/src/components/canvas/Evaluation/Evaluations.tsx
+++ c3an-building/src/components/canvas/Evaluation/Evaluations.tsx
@@ -1,4 +1,5 @@
-import React, { Children } from "react";
+
+import * as React from "react";
 import  Sidebar from "../panels/Sidebar";
 import  AgentBlock  from "../AgentBlock";
 import ToolNode from "../ToolNode"
